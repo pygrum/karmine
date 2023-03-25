1a3a62c161266a40242dc95bd6759546f3a34a2f
py <pygrum.sec@gmail.com>
2023-03-25 09:30:52 -0700

    removed cmd binaries

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+set -e
+
+# Use this script to rebuild the commands if any changes were made post-installation
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $SCRIPTPATH
+
+if [ ! -d "../../kbin" ]; then
+    mkdir "../../kbin"
+fi
+
 for i in $(ls); do
     if [ ! -f $i ]; then
         cd $i && 
         go build && 
-        cp $i ../../kbin && 
-        mv $i ${HOME}/.kbin && cd ..;
+        mv $i ../../kbin && cd ..;
     fi
 done
+
+if [ -d "${HOME}/.kbin" ]; then
+    rm -rf "${HOME}/.kbin"
+fi
+
+mv "../../kbin" ${HOME}/.kbin
